fa883dc36d2cb3f7908ddf150cdc645148dedd2a
Toutzn <torsten@wilms.ac>
2025-12-11 12:58:36 +0100

    tag to amd64

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 
 DOCKER_RUN_IMAGE=nagios
 
-docker build -t "${DOCKER_RUN_IMAGE}" .
+docker build --platform=linux/amd64 -t "${DOCKER_RUN_IMAGE}" .
 
 docker images
 docker run -d --rm --name "${DOCKER_RUN_IMAGE}" -p 8080:80 -t "${DOCKER_RUN_IMAGE}"
